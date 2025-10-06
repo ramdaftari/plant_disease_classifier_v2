97ad7088216771182163018d3738c219dd2142a8
Pradyumn Vikram <pradyumn.vik@gmail.com>
2025-10-06 16:34:53 +0530

    layout modified

--- application_studio/app/src/main/java/com/example/plantdiseaseclassifier/MainActivity.kt
+++ application_studio/app/src/main/java/com/example/plantdiseaseclassifier/MainActivity.kt
@@ -11,12 +11,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.plantdiseaseclassifier.databinding.ActivityMainBinding
 import com.example.plantdiseaseclassifier.ui.theme.PlantDiseaseClassifierTheme
 
 class MainActivity : ComponentActivity() {
+    private val mainBinding : ActivityMainBinding by lazy{
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        enableEdgeToEdge()
+        setContentView(mainBinding.root)
     }
 }
