fa186d29adafbeae1692dee85dbd958996145d4f
Pradyumn Vikram <pradyumn.vik@gmail.com>
2025-10-14 03:30:03 +0530

    updated layout

--- application_studio/app/build.gradle.kts
+++ application_studio/app/build.gradle.kts
@@ -51,6 +51,8 @@ dependencies {
 
     implementation("androidx.camera:camera-view:${camerax_version}")
     implementation("androidx.camera:camera-extensions:${camerax_version}")
+
+    implementation("com.google.android.material:material:1.8.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
